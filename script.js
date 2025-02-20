473c5db3d98def128b66b21869ee34870e6e76cd
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-02-20 14:37:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let container = document.querySelector(".container");
-  let quizContainer = document.querySelector(".quizContainer");
-  let quiz = [
+  let quizData = [
     {
       question: "Which language runs in a web browser?",
-      a: "Java",
-      b: "C",
-      c: "Python",
-      d: "JavaScript",
-      correctAnswer: "d",
+      options: ["Java", "C", "Python", "JavaScript"],
+
+      correctAnswer: 3,
     },
     {
       question: "What does CSS stand for?",
-      a: "Central Style Sheets",
-      b: "Cascading Style Sheets",
-      c: "Cascading Simple Sheets",
-      d: "Cars SUVs Sailboats",
-      correctAnswer: "b",
+      options: [
+        "Central Style Sheets",
+        "Cascading Style Sheets",
+        "Cascading Simple Sheets",
+        "Cars SUVs Sailboats",
+      ],
+
+      correctAnswer: 1,
     },
+
     {
       question: "What does HTML stand for?",
-      a: "Hypertext MarkUp Language",
-      b: "Hypertext MarkDown Language",
-      c: "Hyperloop Markup Language",
-      d: "Helicopter Terminals Motorboats Lamborgini",
-      correctAnswer: "a",
+      options: [
+        "Hypertext MarkUp Language",
+        "Hypertext MarkDown Language",
+        "Hyperloop Markup Language",
+        "Helicopter Terminals Motorboats Lamborgini",
+      ],
+      correctAnswer: 0,
     },
     {
       question: "What year was JavaScript launched?",
-      a: "1996",
-      b: "1995",
-      c: "1994",
-      d: "None of the above",
-      correctAnswer: "b",
+      options: ["1996", "1995", "1994", "None of the above"],
+
+      correctAnswer: 1,
     },
   ];
 
-  let ques=document.createElement('h1')
-  ques.classList.add('question')
+  let [questions, optionA, optionB, optionC, optionD] =
+    document.querySelectorAll(".question,#optionA,#optionB,#optionC,#optionD");
+  let btn = document.querySelector("btn");
+  const curentQuiz = 0;
+  const score = 0;
 
-  
+  function loadData() {
+    const { question, options } = quizData[curentQuiz];
+    questions.innerText = question;
+    options.forEach(
+      (currOptions, index) =>
+        (window[`option${index + 1}`].innerHTML=currOptions)
+    );
+  }
 
+  loadData();
 });
